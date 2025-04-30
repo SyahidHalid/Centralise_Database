56584dd0d070edac806e198b95c0aba4ed0104f1
SyahidHalid <syahidhalid@gmail.com>
2025-04-30 14:32:58 +0800

    Create Calculation_ECL.py

---
+++ ITD/Calculation_ECL.py
@@ -0,0 +1,22 @@
+import pandas as pd
+import numpy as np
+import pyodbc
+import datetime as dt
+
+pd.set_option("display.max_columns", None) 
+pd.set_option("display.max_colwidth", 1000) #huruf dlm column
+pd.set_option("display.max_rows", 100)
+pd.set_option("display.precision", 2) #2 titik perpuluhan
+
+conn = pyodbc.connect(
+    'DRIVER={ODBC Driver 17 for SQL Server};'
+    'SERVER=10.20.1.19,1455;'
+    'DATABASE=mis_db_prod;'
+    'UID=mis_admin;'
+    'PWD=Exim1234;'
+    'Encrypt=yes;TrustServerCertificate=yes;'
+)
+
+cursor = conn.cursor()
+
+A = pd.read_sql_query("SELECT * FROM view_ecl_report_data", conn)
